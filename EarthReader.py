0df360bff76d5629da13a1fc298ad882c53b9e07
Hong Minhee <minhee@dahlia.kr>
2014-05-21 03:59:30 +0900

    Write logs to a file

--- EarthReader.py
+++ EarthReader.py
@@ -1,5 +1,8 @@
 import logging
+import os
+import os.path
 
+from Foundation import NSLog
 from objc import setVerbose
 from PyObjCTools import AppHelper
 
@@ -8,12 +11,20 @@ from earthreader.mac.main import main
 
 
 if __name__ == '__main__':
-    handler = NSLogHandler()
-    handler.setLevel(logging.DEBUG)  # FIXME
+    formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
+    debug_handler = NSLogHandler()
+    debug_handler.setLevel(logging.DEBUG)  # FIXME
+    logfile_path = os.path.join(os.environ['HOME'], 'Library', 'Logs',
+                                'EarthReader.log')
+    file_handler = logging.FileHandler(logfile_path)
+    file_handler.setLevel(logging.DEBUG)  # FIXME
+    file_handler.setFormatter(formatter)
     for logger_name in 'earthreader', 'libearth':
         logger = logging.getLogger(logger_name)
-        logger.addHandler(handler)
+        logger.addHandler(debug_handler)
+        logger.addHandler(file_handler)
         logger.setLevel(logging.DEBUG)  # FIXME
+    NSLog('Logs will go to ' + logfile_path)
     main()
     setVerbose(1)
     AppHelper.runEventLoop()
